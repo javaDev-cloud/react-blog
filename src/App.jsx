78abf256725fed846040fbfcc8a2a6d9c4d7b636
javaDev-cloud <munish6330@gmail.com>
2025-09-19 14:05:33 -0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header />
+      <h1> Hello React</h1>
+      <h1>code By Example</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <>
+      <h1>Header file component.</h1>
+    </>
+  );
+}
+
+export default Header;
