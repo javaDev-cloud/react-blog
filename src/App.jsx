e274b38b7fd8acd80f8a8c468ba8f270251e3619
javaDev-cloud <munish6330@gmail.com>
2025-09-22 11:45:12 -0400

    react-props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
+  let name = "munish";
+  let userObj = { name: "munish", age: "30", email: "munish@test.com" };
+  let users = [
+    { name: "munish", age: "30", email: "munish@test.com" },
+    { name: "kamal", age: "35", email: "kamal@test.com" },
+    { name: "nath", age: "38", email: "nath@test.com" },
+  ];
   return (
     <>
       <h1>React-Props</h1>
+      {/* <Header name={name} /> */}
+      <Header user={users} />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
-function Header() {
+function Header({ user }) {
+  console.log({ user });
   return (
     <>
-      <h1>Header file component.</h1>
+      <h2> Header file component: </h2>;
+      {user.map((d, index) => {
+        return (
+          <h3 id={index}>
+            Name: {d.name} Age: {d.age} Email: {d.email}
+          </h3>
+        );
+      })}
+      ;
     </>
   );
 }
