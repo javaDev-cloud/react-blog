6f64b3d1c618f8e9527af167e7a1102ba5a92591
javaDev-cloud <munish6330@gmail.com>
2025-09-22 11:00:47 -0400

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./components/Header";
-import Login from "./components/Login";
-import { Profile, userDetails } from "./components/Login";
-import Counter from "./components/counter";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
-      <Header />
-      <Login />
-      <Profile />
-      <Counter />
-      <h1>{userDetails}</h1>
-      <h1> Hello React</h1>
-      <h1>code By Example</h1>
+      <h1>React-Props</h1>
     </>
   );
 }
